b6cb5df25a19abcbc53aaa79727409eb9ce5741d
Andrey <andvikt@gmail.com>
2019-10-13 21:27:39 +0300

    new: project description for pypi

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from setuptools import setup
 with open('VERSION', 'r') as f:
     version = f.read()
 
+with open('README.md', 'r') as f:
+    desc = f.read()
+
 setup(
     name='pro_lambda',
     version=version,
     packages=['pro_lambda'],
-    url='',
-    license='',
+    url='https://github.com/andvikt/pro_lambda',
+    license='MIT',
     author='Viktorov A.G.',
     author_email='andvikt@gmail.com',
     description='Lambda with math operators support',
     tests_require=['pytest', 'pytest_asyncio'],
+    long_description=desc,
+    long_description_content_type='text/markdown',
 )
