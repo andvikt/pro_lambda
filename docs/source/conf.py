805d0bc22a692e86e9ff2a781df61eca5b3db7df
Andrey <andvikt@gmail.com>
2019-10-20 10:56:51 +0300

    Bump version: 0.3.1 → 0.3.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Viktorov A.G.'
 author = 'Viktorov A.G.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
